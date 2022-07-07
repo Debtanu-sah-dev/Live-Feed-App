e5d5e91b02fb5a547ddb6d67adb0ffb305251605
Debtanu sah dev <debtanucoder@gmail.com>
2022-07-07 21:10:33 +0530

    🚀 Commited

---
+++ js/index.js
@@ -0,0 +1,119 @@
+// Stream server For Peer To Peer Connection
+
+const servers = {
+    iceServers: [{
+        urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302"
+        ]
+    }],
+    iceCandidatePoolSize: 10
+}
+
+//Global Stream
+
+let PeerConnection = new RTCPeerConnection(servers);
+let localStream = null;
+
+// DOM Elements
+
+const video = document.querySelector('#video');
+const link = document.querySelector('[data-link]');
+const list = document.querySelector('[data-list]');
+const copy = document.querySelector('[data-copy]');
+const camera = document.querySelector('[data-camera]');
+const connectlink = document.querySelector('[data-connectlink]');
+let isCamerOn = false;
+
+async function updateList(cameras){
+    list.innerHTML = "";
+    let arr = await cameras;
+    arr.map((e) => {
+        const option = document.createElement('option');
+        option.label = e.label;
+        option.value = e.deviceId;
+
+        return option;
+    }).forEach((cameraOption) => {
+        list.appendChild(cameraOption)
+    })
+}
+
+async function getCameras() {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    return devices.filter(device => device.kind === "videoinput")
+}
+
+camera.addEventListener("click", async function () {
+    localStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: {
+            deviceId: list.value
+        }
+    });
+
+    localStream.getTracks().forEach(track => {
+        PeerConnection.addTrack(track, localStream);
+    });
+
+    video.srcObject = localStream;
+    video.play();
+
+    isCamerOn = true;
+})
+
+list.addEventListener("change", async function () {
+    camera.click();
+});
+
+connectlink.addEventListener("click", async function () {
+    if(isCamerOn){
+        connectlink.style.display = "none";
+        const feedDoc = db.collection("feeds").doc();
+        const offer = feedDoc.collection("offer");
+        const candidate = feedDoc.collection("candidate");
+        link.innerText = feedDoc.id;
+
+        PeerConnection.onicecandidate = event => {
+            event.candidate && offer.add(event.candidate.toJSON())
+        }
+    
+    
+        const offerDescriptor = await PeerConnection.createOffer();
+        await PeerConnection.setLocalDescription(offerDescriptor);
+    
+        await feedDoc.set({
+            offer:{
+                sdp: PeerConnection.localDescription.sdp,
+                type: PeerConnection.localDescription.type
+            }
+        });
+
+        feedDoc.onSnapshot(async (snapshot) => {
+            const data = snapshot.data();
+            if(!PeerConnection.currentRemoteDescription && data?.answer){
+                let remDesc = new RTCSessionDescription(data.answer);
+                await PeerConnection.setRemoteDescription(remDesc);
+            }
+        })
+
+        candidate.onSnapshot(async (snapshot) => {
+            snapshot.docChanges().forEach(async (change) => {
+                if(change.type === "added"){
+                    let ICE = new RTCIceCandidate(change.doc.data());
+                    await PeerConnection.addIceCandidate(ICE)
+                }
+            })
+        })
+    }
+    else{
+        alert("Please open your camera first");
+        camera.click()
+    }
+})
+
+const newList = getCameras();
+updateList(newList);
---
+++ js/remote.js
@@ -0,0 +1,95 @@
+// Stream server For Peer To Peer Connection
+
+const servers = {
+    iceServers: [{
+        urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302"
+        ]
+    }],
+    iceCandidatePoolSize: 10
+}
+
+//Global Stream
+
+let PeerConnection = new RTCPeerConnection(servers);
+let remoteStream = new MediaStream();
+
+
+//DOM Elements
+
+const video = document.querySelector('#video');
+const link = document.querySelector('[data-link]');
+const connecter = document.querySelector('[data-connecter]');
+
+
+
+PeerConnection.ontrack = event => {
+    event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
+    });
+}
+
+async function connect() {
+    try {
+        const feedId = link.value;
+        const feedDoc = db.collection("feeds").doc(feedId);
+        const offer = feedDoc.collection("offer");
+        const answer = feedDoc.collection("candidate");
+
+        video.srcObject = remoteStream;
+        video.play();
+
+        PeerConnection.onicecandidate = event => {
+            event.candidate && answer.add(event.candidate.toJSON())
+        }
+
+        let callData = await feedDoc.get();
+
+        const offerer = new RTCSessionDescription(callData.data().offer);
+        await PeerConnection.setRemoteDescription(offerer);
+
+        let answerer = await PeerConnection.createAnswer();
+        await PeerConnection.setLocalDescription(answerer);
+        feedDoc.update({
+            answer:{
+                sdp: answerer.sdp,
+                type: answerer.type
+            }
+        })
+
+        offer.onSnapshot(async function (snapshot) {
+            snapshot.docChanges().forEach(async function (change) {
+                if (change.type === "added") {
+                    const candidate = new RTCIceCandidate(change.doc.data());
+                    await PeerConnection.addIceCandidate(candidate);
+                }
+            })
+        })
+
+
+
+        // if(feedDoc){
+        //     throw new Error();
+        // }
+        // connecter.style.display = "none";
+        // let remDesc = new RTCSessionDescription(await feedDoc.get().data().sdp);
+        // await PeerConnection.setRemoteDescription(remDesc);
+        // let answer = await PeerConnection.createAnswer();
+        // await PeerConnection.setLocalDescription(answer);
+    } catch (e) {
+        console.error(e);
+        alert("Invalid Link");
+    }
+}
+
+connecter.addEventListener("click", connect);
+
+//Initialize link
+if (new URLSearchParams(location.search).get("c") != null) {
+    link.value = new URLSearchParams(location.search).get("c");
+    connect();
+}
